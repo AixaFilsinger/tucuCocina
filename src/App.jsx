bc7dd2bab999d20186ddb39e508acb82bd284e2f
Aixa Filsinger <macafil99@gmail.com>
2025-05-04 10:31:31 -0300

    se maqueto la pagina registrarse

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Menu from './components/common/Menu'
+import Registrarse from './components/views/Registrarse';
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Menu></Menu>
+      <Registrarse></Registrarse>
     </>
   )
 }
