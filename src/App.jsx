977fbcbe442ff897b8def5e4ce8acb1bedf32e44
Aixa Filsinger <macafil99@gmail.com>
2025-05-05 19:53:58 -0300

    se maqueto el home de usuario para poder hacer el crud

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <Menu></Menu>
-      <CardHomeReceta></CardHomeReceta>
+      <UsuarioHome></UsuarioHome>
     </>
   )
 }
--- src/components/views/UsuarioHome.jsx
+++ src/components/views/UsuarioHome.jsx
@@ -1,11 +1,14 @@
-import CardHomeReceta from "./receta/CardHomeReceta";
-import ContenedorRecetaCards from "./receta/ContenedorRecetaCards";
+import { PlusCircle } from "react-bootstrap-icons";
 
 const UsuarioHome = () => {
     return (
-        <section>
-            <ContenedorRecetaCards cardHome={CardHomeReceta}>
-            </ContenedorRecetaCards>
+        <section className="mainSection">
+            <article className="d-flex justify-content-center align-items-center mt-5">
+                 <h2 className="me-5">Tus Recetas</h2>
+           
+            <button className="d-flex align-items-center">Nueva Receta <PlusCircle className=" ms-1 fs-4"></PlusCircle></button>
+            </article> <hr className="mx-5" />
+           
         </section>
     );
 };
